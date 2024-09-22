8c2e9e5eccaf794633f38640fd2f877286cf10f2
Carlos Antonio <carlosantonioob@gmail.com>
2024-09-22 10:38:05 -0300

    Alteração nomes de arquivos

---
+++ 005 - programa que leia um numero e mostre seu sucessor e seu antecessor.py
@@ -0,0 +1,3 @@
+#crie um programa que leia um numero e mostre seu sucessor e seu antecessor
+n = int(input('digite um número: '))
+print(f'Analisando o valor {n}, seu antecessor é {n-1} e o sucessor é {n+2}')
